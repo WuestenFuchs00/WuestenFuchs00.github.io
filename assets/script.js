0b3cea38091303f32da523c4a874c3424683ebfa
WuestenFuchs00 <iironbreaker@gmail.com>
2018-11-01 18:32:09 +0100

    Update

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,62 @@
-<!--
-	function clearInput () {
-		document.getElementById('mDivElektronenkonfigurationAusgabe').innerHTML = "NaN";
-		document.getElementById('mInputElement').value = "";
-	}
-	
-	function setOpacity (mObj, mOpac) { /* mOpac is a numbre between 0% and 100% */
-		mObj.style.opacity = "" + (mOpac/100);
-		mObj.style.filter = "alpha(opacity=" + mOpac + ")";
-	}
-	
-	function printElektronenkonfiguration () {
-		var strAusgabe = Elektronenkonfiguration(document.getElementById('mInputElement').value);		
-		var divAusgabe = document.getElementById('mDivElektronenkonfigurationAusgabe');
-		var mOpac = 0, mId = 0;
-		
-		setOpacity(divAusgabe, mOpac);
-		divAusgabe.innerHTML = strAusgabe.replace(/\(/g, "<sup>").replace(/\)/g, "</sup>&nbsp;");
-		
-		mId = window.setInterval(function () {
-			if ( mOpac >= 100 ) {
-				setOpacity(divAusgabe, 100);
-				clearTimeout(mId);
-				return;
+/**
+ * script.js
+ */ 
+var mJS = (function () {
+	return {
+		/*
+		* Provides the current window size. If the window is resized by the user,
+		* this function also returns the updated window size.
+		*/
+		mGetWindowSize : function () {
+			var w = window, b = document.body, e = document.documentElement
+				width = w.innerWidth || b.clientWidth || e.clientWidth,
+				height = w.innerHeight || b.clientHeight || e.clientHeight;
+			return {width, height};
+		}, // mGetWindowSize
+		/*
+		* Adjust the (recently loaded) image to fit to the new window size.
+		*/
+		mResizeImg : function (o) {
+			// Get image size
+			var imgWidth = o.getBoundingClientRect().width,
+				imgHeight = o.getBoundingClientRect().height;
+			// Calculate (new) image size, related to (current) window size
+			var offset = 5*6; // from css style, margin left/right, factor 5x
+			var windowSize = mGetWindowSize();
+			var newImgWidth = ( imgWidth > windowSize.width ) ? windowSize.width : imgWidth;
+			newImgWidth -= offset;
+			var newImgHeight = ( imgWidth > windowSize.width ) ? (imgHeight / imgWidth) * newImgWidth : imgHeight;
+			// Set image size
+			o.setAttribute("style", "width:" + newImgWidth + "px");
+			o.setAttribute("style", "height:" + newImgHeight + "px");
+		}, // mResizeImg
+		/*
+		 *
+		 */
+		mScaleBoxes : function () {
+			// Get number of topic boxes
+			var mBoxes = document.querySelectorAll("div.mTopic");
+			if ( mBoxes.length <= 0 ) return; // Exit
+			// Get total width of all boxes
+			var mOffset = 10; // const margin top/bottom left/right
+			var mTotalW = mBoxes.length * ( mOffset + mBoxes[0].getBoundingClientRect().width );
+			// Get (current) window width
+			var mWindowWidth = this.mGetWindowSize().width;
+			// Calculate section height
+			var mSectionHeight = 0;			
+			var mL = 1;
+			if ( mTotalW < mWindowWidth ) {
+				mSectionHeight = mBoxes[0].getBoundingClientRect().height + 3 * mOffset;
+			} else {
+				mL = Math.ceil(mTotalW / mWindowWidth);
+				mSectionHeight = (mBoxes[0].getBoundingClientRect().height * mL) + 3.6 * mOffset;
 			}
-			mOpac += 5;
-			setOpacity(divAusgabe, mOpac);
-		}, 50);
+			// Set section height
+			var mSection = document.querySelectorAll("div.mSection")[0];
+			mSection.setAttribute("style", "height:" + mSectionHeight + "px");
+		}, // mScaleBoxes
 	}
--->
+})();
+
+// document.addEventListener("DOMContentLoaded", () => mJS.mScaleBoxes(), false);
+document.addEventListener("DOMContentLoaded", function () { mJS.mScaleBoxes(); }, false);
